54e35745b55d4946759d5cf37e4ce23e586c3b76
harryMontenegro <harryMontenegro@gmail.com>
2022-06-16 00:44:53 -0500

    Ejercicio mesa de trabajo C14S

---
+++ C14S/src/alquilerembarcacion/Velero.java
@@ -0,0 +1,23 @@
+package alquilerembarcacion;
+
+public class Velero extends Embarcacion{
+
+    private Integer cantidadMastil;
+
+    public Velero(Double precioBase, Double valorAdicional, Integer anioFabricacion, Integer eslora, Capitan capitan, Integer cantidadMastil) {
+        super(precioBase, valorAdicional, anioFabricacion, eslora, capitan);
+        this.cantidadMastil = cantidadMastil;
+    }
+
+    public Integer getCantidadMastil() {
+        return cantidadMastil;
+    }
+
+    public void setCantidadMastil(Integer cantidadMastil) {
+        this.cantidadMastil = cantidadMastil;
+    }
+
+    public boolean evaluarTamanio(){
+        return cantidadMastil > 4;
+    }
+}
